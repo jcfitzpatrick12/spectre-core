a538ebe40f5797aec6cfe416bafacfa35acc29f1
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2025-03-24 19:17:46 +0000

    fix: keep mypy happy

--- src/spectre_core/spectrograms/_transform.py
+++ src/spectre_core/spectrograms/_transform.py
@@ -71,17 +71,17 @@ def time_chop(
     :raises ValueError: If the start and end indices for the time range are identical.
     :return: A new spectrogram containing only the specified time range.
     """
-    start_datetime = np.datetime64(start_datetime)
-    end_datetime   = np.datetime64(end_datetime)
+    start_datetime64 = np.datetime64(start_datetime)
+    end_datetime64   = np.datetime64(end_datetime)
 
-    is_entirely_below_time_range = (start_datetime < spectrogram.datetimes[0] and end_datetime < spectrogram.datetimes[0])
-    is_entirely_above_time_range = (start_datetime > spectrogram.datetimes[-1] and end_datetime > spectrogram.datetimes[-1])
+    is_entirely_below_time_range = (start_datetime64 < spectrogram.datetimes[0] and end_datetime64 < spectrogram.datetimes[0])
+    is_entirely_above_time_range = (start_datetime64 > spectrogram.datetimes[-1] and end_datetime64 > spectrogram.datetimes[-1])
     if is_entirely_below_time_range or is_entirely_above_time_range:
         raise ValueError(f"Requested time interval is entirely out of range of the input spectrogram.")
     
     # find the index of the nearest matching spectrums in the spectrogram.
-    start_index = find_closest_index(start_datetime, spectrogram.datetimes)
-    end_index   = find_closest_index(end_datetime, spectrogram.datetimes)
+    start_index = find_closest_index(start_datetime64, spectrogram.datetimes)
+    end_index   = find_closest_index(end_datetime64, spectrogram.datetimes)
     
     # enforce distinct start and end indices
     if start_index == end_index:
