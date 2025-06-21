5ba68fe5fdd103100f53b250355145da5140e0f2
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2025-06-21 15:24:34 +0000

    feat: Expose `BasePanel` publically

--- src/spectre_core/plotting/__init__.py
+++ src/spectre_core/plotting/__init__.py
@@ -5,6 +5,7 @@
 """An intuitive API for plotting spectrogram data."""
 
 from ._format import PanelFormat
+from ._base import BasePanel
 from ._panels import (
     SpectrogramPanel,
     FrequencyCutsPanel,
@@ -14,6 +15,7 @@ from ._panels import (
 from ._panel_stack import PanelStack
 
 __all__ = [
+    "BasePanel",
     "PanelFormat",
     "PanelStack",
     "SpectrogramPanel",
