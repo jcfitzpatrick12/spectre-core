1ebda2a8b68875b4bcbff8aa0ab80f09faea02c5
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2025-06-25 16:44:23 +0000

    chore: Enforce black formatting

--- src/spectre_core/receivers/plugins/_custom.py
+++ src/spectre_core/receivers/plugins/_custom.py
@@ -8,11 +8,13 @@ from .._register import register_receiver
 from .._receiver import Receiver
 from ._receiver_names import ReceiverName
 
+
 @register_receiver(ReceiverName.CUSTOM)
 class CustomReceiver(Receiver):
     """A receiver which starts with no operating modes.
-    
+
     Customise by adding modes using the `add_mode` method.
     """
+
     def __init__(self, name: ReceiverName, mode: Optional[str] = None) -> None:
         super().__init__(name, mode)
