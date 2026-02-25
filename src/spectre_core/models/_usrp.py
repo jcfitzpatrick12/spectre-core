ea6bfd8f942f0e18b31c2ca7ac303b8f7acbcbce
Luis M. B. Varona <luismbvarona@protonmail.com>
2026-02-24 20:21:22 -0400

    Add missing 'wire_format'/'output_type' validation to swept frequency modes
    In both 'B200miniSweptCenterFrequency' and 'USRPSweptCenterFrequency', calls to 'validate_wire_format' and 'validate_output_type' were missing from the 'validator' method, despite being present in their respective 'fixed_center_frequency' counterparts. This allowed invalid values to pass through without error in 'swept_center_frequency' mode.
    
    (Resolves jcfitzpatrick12/spectre#197.)

--- src/spectre_core/models/_usrp.py
+++ src/spectre_core/models/_usrp.py
@@ -60,4 +60,6 @@ class USRPSweptCenterFrequency(
         validate_sample_rate_with_master_clock_rate(
             self.sample_rate, self.master_clock_rate
         )
+        validate_wire_format(self.wire_format)
+        validate_output_type(self.output_type)
         return self
